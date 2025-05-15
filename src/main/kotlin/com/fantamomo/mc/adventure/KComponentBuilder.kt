f028fd883dd74fc48d5271a1efe592de93ae4340
Fantamomo <git@fantamomo.com>
2025-05-15 13:37:16 +0200

    Add KComponentBuilder interface for component building
    Introduces a Kotlin-specific interface to streamline building Adventure components. This includes support for appending components and ensures compatibility with Adventure's ComponentBuilder.

---
+++ src/main/kotlin/com/fantamomo/mc/adventure/KComponentBuilder.kt
@@ -0,0 +1,16 @@
+package com.fantamomo.mc.adventure
+
+import net.kyori.adventure.text.BuildableComponent
+import net.kyori.adventure.text.ComponentBuilder
+import net.kyori.adventure.text.ComponentLike
+
+@ComponentDsl
+interface KComponentBuilder<C : BuildableComponent<C, B>, B : ComponentBuilder<C, B>> : Styleable<B> {
+    override val builder: B
+
+    fun build(): C
+
+    fun append(component: ComponentLike) {
+        builder.append(component)
+    }
+}
