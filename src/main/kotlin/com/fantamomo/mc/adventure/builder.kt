43777aa62dbea99742361b0ac77772cad59893da
Fantamomo <git@fantamomo.com>
2025-05-15 14:35:51 +0200

    feat: add styleBuilder function for building text styles

--- src/main/kotlin/com/fantamomo/mc/adventure/builder.kt
+++ src/main/kotlin/com/fantamomo/mc/adventure/builder.kt
@@ -1,6 +1,7 @@
 package com.fantamomo.mc.adventure
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.Style
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
@@ -9,6 +10,12 @@ import kotlin.contracts.contract
 fun textComponent(block: KTextComponent.() -> Unit): Component {
     contract { callsInPlace(block, InvocationKind.EXACTLY_ONCE) }
     val builder = KTextComponent()
+    builder.block()
+    return builder.build()
+}
+
+fun styleBuilder(block: KStyle.() -> Unit): Style {
+    val builder = KStyle()
     builder.block()
     return builder.build()
 }
